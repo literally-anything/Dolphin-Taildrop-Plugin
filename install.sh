fc019d2f5040d4f3f53a0b315173192e0b977183
Hunter Baker <62899372+literally-anything@users.noreply.github.com>
2024-03-17 11:24:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -90,7 +90,7 @@ except:
     pass
 
 proc = subprocess.Popen(
-    ['notify-send', '-a', 'TailDrop', 'Recieved', f'\"{name}\"', '-i', f'\"{path}\"', '-A', '\"Show in Dolphin\"'],
+    ['notify-send', '-a', 'TailDrop', '-i', f'"{path}"', '-A', '"Show in Dolphin"', 'Recieved', f'"{name}"',],
     stdout=subprocess.PIPE
 )
 stdout = proc.communicate()[0].strip()
